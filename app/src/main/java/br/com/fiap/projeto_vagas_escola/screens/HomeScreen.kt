ccf3deab3903c674163a07dbc19cf5e35b6e474f
arthurwrx <arthurwrx@outlook.com>
2024-03-16 20:49:40 -0300

    Separei as Columns das imagens, falta criar a barra de baixo

--- app/src/main/java/br/com/fiap/projeto_vagas_escola/screens/HomeScreen.kt
+++ app/src/main/java/br/com/fiap/projeto_vagas_escola/screens/HomeScreen.kt
@@ -1,21 +1,12 @@
 package br.com.fiap.projeto_vagas_escola.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,64 +15,75 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import br.com.fiap.projeto_vagas_escola.R
 import br.com.fiap.projeto_vagas_escola.component.Header
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun HomeScreen() {
-
-    //Box inicial com logo e titulo
     Box(
-        modifier = Modifier
-            .fillMaxWidth()
+        modifier = Modifier.fillMaxSize()
     ) {
         LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .background(Color.White),
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp)
         ) {
             item {
                 Header()
-            }//Medoto que ira retornar um layout para o Header
+            }
 
-            //Card para cadastrar um usuario
             item {
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 32.dp, vertical = 50.dp)
+                    modifier = Modifier.fillMaxWidth()
                 ){
-                    Card(modifier = Modifier
-                        .fillMaxWidth(),
-                        colors = CardDefaults.cardColors(Color.White),
-                        elevation = CardDefaults.cardElevation(defaultElevation = 16.dp),
-                        shape = RoundedCornerShape(16.dp)
+                    OutlinedTextField(
+                        value = "",
+                        onValueChange = {},
+                        placeholder = {
+                            Text(
+                                text = "Digite aqui o endereço da escola desejada:",
+                                fontSize = 14.sp
+                            )
+                        },
+                        modifier = Modifier.fillMaxWidth(),
+                        trailingIcon = {
+                            IconButton(onClick = { /* Ação de clique do ícone */ }) {
+                                Icon(
+                                    imageVector = Icons.Filled.Search,
+                                    contentDescription = "Pesquisar"
+                                )
+                            }
+                        }
                     )
-                    {
-                        Text(text = "  Digite aqui o endereço da escola desejada:",
-                            fontSize = 14.sp,
-                        )
-                    }
-                    Spacer(modifier = Modifier.height(25.dp))
+                }
+            }
+
+            item {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
                     Image(
                         painter = painterResource(id = R.drawable.escola),
                         contentDescription = "logo",
                         modifier = Modifier
-                            .size(500.dp)
-                            .padding(top = 16.dp)
+                            .size(500.dp),
+                        contentScale = ContentScale.FillWidth
                     )
-                        }
-                    }
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(text = "Arthur Morgan")
+                    Text(text = "Dutch")
                 }
             }
         }
-
+    }
+}
 
 @Preview
 @Composable
-private fun HomeScreenPreview() {
+fun HomeScreenPreview() {
     HomeScreen()
 }
